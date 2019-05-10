57628fe994b231c4e504d7e7520d5861fe0944d2
ahargey <3650393@myuwc.ac.za>
2019-05-10 11:30:08 +0200

    #mites, dunes and doubs

---
+++ PCA.R
@@ -0,0 +1,24 @@
+#AYESHA HARGEY
+#3650393
+#PCA
+
+#Load libraries
+library(tidyverse)
+library(vegan)
+
+env <- read_csv("~/Quant/Textbook/NEwR_1st_edition_R332/NEwR_updated_material_R332-NEwR_ed1/DoubsEnv.csv")
+View(env)
+
+#PCA
+env.pca <- rda(env, scale = TRUE)
+env.pca
+
+cor.env <- cor(env) #unconstrained and inertia is the same because it's an unconstrained
+
+summary(env.pca)
+
+biplot(env.pca, scaling = 1, choices = c(1, 2), main = "PCA - scaling 1")
+#shortest arrow has the lowest value eg. in this case, pca
+
+
+#do this on the doubs species data
