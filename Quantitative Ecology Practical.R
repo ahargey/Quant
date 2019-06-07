daa9c3e28393b9eec0d49e4fb77198697284af50
ahargey <3650393@myuwc.ac.za>
2019-06-07 23:15:11 +0200

    #final test

--- Quantitative Ecology Practical.R
+++ Quantitative Ecology Practical.R
@@ -20,6 +20,8 @@ spa <- read_delim("Final Spatial.csv", ";",
                   escape_double = FALSE, trim_ws = TRUE)
 spe <- read_delim("Final Species.csv", ";", 
                   escape_double = FALSE, trim_ws = TRUE)
+spe_raw <- read_delim("Final Species.csv", ";", 
+                  escape_double = FALSE, trim_ws = TRUE)
 
 spe <- spe %>% mutate_all(funs(replace_na(.,0))) #turns NA values into 0 for the species table
 
@@ -30,6 +32,8 @@ tail(spe) #last six rows
 nrow(spe) #number of row
 ncol(spe) #number of columns
 summary(spe) #gives a summary of the mean, median, quartiles and min/max values
+sum(spe == 0) #number of absences
+sum(spe == 0) / (nrow(spe) * ncol(spe)) #proportion of zeros
 
 glimpse(env) 
 head(env) 
@@ -43,39 +47,60 @@ spe <- spe[c(-5, -10, -16, -27), ]
 env <- env[c(-5, -10, -16, -27), ]
 spa <- spa[c(-5, -10, -16, -27), ]
 
-env_test <- env %>% 
+env <- env %>% 
   select(-X1)
 
-# Hellinger pre-transformation of the species data
-spe_h <- spe %>% 
-  select(-X1) #removing the named column
-spe_h <- decostand(spe_h, "hellinger")
-#Hellinger transformation done as this is species abundance data.
-#It gives low weights to species with minimal observations.
+spe <- spe %>% 
+  select(-X1)
 
-#CCA
-spe_cca <- cca(spe_h ~ env_test$dfs)
-summary(spe_cca)
-
-# The adjusted R2 --- the variance explained by the constrained axes
-(spe_cca_R2a <- RsquareAdj(spe_cca)$adj.r.squared)
-
-# Variance explained by full model
-sum(spe_cca$CCA$eig) / spe_cca$tot.chi * 100
-
-anova(spe_cca)
-
-DoubsSpe.cca.axis.test <- anova(spe_cca, by = "term")
-
-#spatial plot
-spa_plot <- ggplot(data = spa, aes(x = X, y = Y)) +
-  geom_path(data = spa, aes(label = X1), size = 0.5, colour = "blue") +
-  geom_text(data = spa, aes(label = X1), size = 4, colour = "red") +
+#spatial plot of quadrats
+spa_plot <- ggplot(data = spa, aes(x = X, y = Y)) + #assigning a name to plot 
+  geom_path(data = spa, aes(label = X1), size = 0.5, colour = "blue") + #path
+  geom_text(data = spa, aes(label = X1), size = 4, colour = "red") + #quadrat names
   annotate("text", label = "Entrance", x = 1, y = 0.5, size = 4.0, angle = 0, colour = "purple") +
-  labs(x = "x coordinate (m)", y = "y coordinate (m)", title = "Quadrat Locations") +
+  annotate("text", label = "Mountain", x = 7, y = 4.5, size = 4.0, angle = 0, colour = "green") +
+  annotate("text", label = "Slope", x = 7, y = 7.8, size = 5.0, angle = 45, colour = "orange") +
+  labs(x = "X coordinate (m)", y = "Y coordinate (m)", title = "Quadrat Locations") +
   theme_bw() +
   theme(panel.border = element_rect(fill = NA, colour = "black", size = 1),
         axis.text = element_text(colour = "black"),
         axis.ticks = element_line(colour = "black"),
         plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
 spa_plot
+
+#CCA analysis is done
+
+spe.cca <- cca(spe ~ ., env) #the period uses all columns in the environmental dataframe
+summary(spe.cca)
+
+# The adjusted R2 --- the variance explained by the constrained axes
+(spe.cca.R2a <- RsquareAdj(spe.cca)$adj.r.squared)
+
+# Variance explained by full model
+sum(spe.cca$CCA$eig) / spe.cca$tot.chi * 100
+
+(spe.cca$CCA$eig[1]/sum(spe.cca$CCA$eig))*100 #the variation explained by the first scaling
+(spe.cca$CCA$eig[2]/sum(spe.cca$CCA$eig))*100 #the variation explained by the second scaling
+
+#Testing to see if this analysis is significant overall
+anova(spe.cca, permutations = how(nperm = 999))
+#It is not, p > 0.05
+#Testing to see if the analysis is significant based on the environmental factors
+anova(spe.cca, by = "axis", permutations = how(nperm = 999))
+#It is not, p > 0.05
+
+vif.cca(spe.cca) #no values over 10 so no redundant contraints 
+
+#Graphs
+
+par(mfrow = c(1, 2)) #parameters of graphs 
+#Scaling 1: species are scored to the relative eigenvalues,
+plot(spe.cca,
+     scaling = 1,
+     display = c("sp", "lc", "cn"),
+     main = "Triplot CCA species and environmental data - scaling 1")
+
+#Scaling 2: site scores scaled to the relative eigenvalues
+plot(spe.cca,
+     display = c("sp", "lc", "cn"),
+     main = "Triplot CCA species and environmental data - scaling 2")
